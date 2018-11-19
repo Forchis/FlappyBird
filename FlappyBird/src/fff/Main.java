136a1a86d46b50ebc248be989614ee21da6f1a61
havelec <havelec@UCEBNA-NB-13.gjkt.local>
2018-11-19 15:35:02 +0100

    22

--- FlappyBird/src/fff/Main.java
+++ FlappyBird/src/fff/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void pozdravy(){
 		System.out.println("Test 1");
 		System.out.println("test 2");
-		System.out.println("Test 2");
+		System.out.println("Test 22335458");//
 	}
 }
