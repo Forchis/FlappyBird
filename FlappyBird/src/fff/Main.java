86123ac712846c9c6996f6cd9cf6681a401f0b91
forche <forche@UCEBNA-NB-14.gjkt.local>
2018-11-26 14:56:26 +0100

    začátek

--- FlappyBird/src/fff/Main.java
+++ FlappyBird/src/fff/Main.java
@@ -1,17 +1,5 @@
 package fff;
 
 public class Main {
-
-	public static void pozdravy(){
-		System.out.println("Test 1");
-		System.out.println("test 2");
-		System.out.println("Test 223");
-	}	
-	public static void pozdravy1() {
-		System.out.println("Test 22335458");//
-	}
-
-	public static void pozdravy2(){
-		
-	}
+	
 }
