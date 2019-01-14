58c286415bd742aa8b5af9eb59792ba49a88eadf
forche <forche@UCEBNA-NB-14.gjkt.local>
2019-01-14 15:19:33 +0100

    blabla2

--- FlappyBird/src/FlappyBird/Postava.java
+++ FlappyBird/src/FlappyBird/Postava.java
@@ -2,12 +2,20 @@ package FlappyBird;
 
 
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
-
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
 
 
@@ -16,16 +24,41 @@ public class Postava {
 	
 	private Image postava;
 	private int xLoc = 0, yLoc = 0;
-		//obrázek postavy 
-	
-	public Postava( int initialWidth, int initialHeight) {
-		//postava = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource());//dodat cestu k obrázku
-		//scaleBird(initialWidth, initialHeight);
 		
+	
+	public Postava( int initialWidth, int initialHeight, Stage theStage) {
+		postava = new Image("/ptak.jpg", initialWidth, initialHeight, false, false);
+		/*postava = new Image("C:/ptak.jpg"); 
+		scaleBird(initialWidth, initialHeight);*/
+		
+		  theStage.setTitle( "Canvas Example" );
+	         
+		    Group root = new Group();
+		    Scene theScene = new Scene( root );
+		    theStage.setScene( theScene );
+		         
+		    Canvas canvas = new Canvas( 400, 200 );
+		    root.getChildren().add( canvas );
+		         
+		    GraphicsContext gc = canvas.getGraphicsContext2D();
+		         
+		    gc.setFill( Color.RED );
+		    gc.setStroke( Color.BLACK );
+		    gc.setLineWidth(2);
+		    Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+		    gc.setFont( theFont );
+		   /* gc.fillText( "Hello, World!", 60, 50 );
+		    gc.strokeText( "Hello, World!", 60, 50 );*/
+		     
+		    //Image earth = new Image( "earth.png" );
+		    gc.drawImage( postava, 180, 100 );
+		         
+		    theStage.show();
 	}
 	
 	public void scaleBird(int width, int height) {
-		//postava = postava.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		//postava = new Image("my/res/flower.png", width, height, false, false);
+		//postava = postava.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
 
 	}
 	
@@ -36,7 +69,7 @@ public class Postava {
 	
 	public int getWidth() {
 		try { 
-				return (int)postava.getWidth(null);
+				return (int)postava.getWidth();
 				
 		}
 		catch(Exception e) {
@@ -46,7 +79,7 @@ public class Postava {
 	
 	public int getHeight() {
 		try {
-				return (int)postava.getHeight(null);
+				return (int)postava.getHeight();
 		}
 		catch(Exception e) {
 			return -1;
@@ -71,13 +104,15 @@ public class Postava {
 	}
 	
 	public Rectangle getRectangle() {
-		return (new Rectangle(xLoc, yLoc,postava.getWidth(null), postava.getHeight(null)));
+		return (new Rectangle(xLoc, yLoc,postava.getWidth(), postava.getHeight()));
 	}
 	
 	public BufferedImage getBI() {
-		BufferedImage bi = new BufferedImage((int)postava.getWidth(null), (int)postava.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		BufferedImage bi = new BufferedImage((int)postava.getWidth(), (int)postava.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		Graphics g = bi.getGraphics();
-		g.drawImage(postava, 0, 0, null);
+		//g.drawImage(postava, 0, 0, null);
+		ImageView iv3 = new ImageView();
+        iv3.setImage(postava);
 		g.dispose();
 		return bi;
 		
